31b0fe01a3d0e4317e0c13b704b9da584fd4b3de
Kevin Sun <kevsun1@gmail.com>
2023-04-15 17:00:53 -0700

    end stream on DONE

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -61,6 +61,8 @@ export default async function generate(req, res) {
           const message = `data: ${JSON.stringify({token: content})}\n\n`;
           console.log(message);
           res.write(`event: message\ndata: ${JSON.stringify({token: content})}\n\n`);
+        } else {
+          res.end();
         }
       } else if (event.type === 'reconnect-interval') {
         console.log('We should set reconnect interval to %d milliseconds', event.value)
